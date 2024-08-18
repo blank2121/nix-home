f2a1c47bcbba2f4f3524cfbff6855c4d4b4ae458
blank2121 <winstonwalter07@gmail.com>
2024-08-18 12:40:47 -0400

    game hash update

--- modules/default/nixos/games.nix
+++ modules/default/nixos/games.nix
@@ -1,8 +1,9 @@
 { config, pkgs, lib, inputs, ... }:
 let
+  #Honkai Star Rail
   aagl-gtk-on-nix = import (builtins.fetchTarball {
     url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-    sha256 = "0pfn9fwjf6m83hk9z8nkmqrwcvd61abshbpw05v656vz5zx621ax";
+    sha256 = "1mnsks7kibjr8kb1cjy2052kjaxrvfp5adg2ibfal7j9862042af";
   });
 in
 {
